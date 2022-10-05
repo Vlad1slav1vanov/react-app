93763ef842179bd5d9f266378f62e64a167bccd0
Vladislav <typlomi@gmail.com>
2022-10-05 14:27:39 +0600

    create Loading hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import Postslist from "./components/Postslist";
 import "./styles/App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/Button/MyButton";
 import { usePosts } from "./hooks/usePosts";
+import PosService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
+import { useFetching } from "./hooks/useFetching";
 
 function App() {
   const [posts, setPosts] = useState([]);
   const [filter, setFilter] = useState({sort: '', query: ''});
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PosService.getAll();
+    setPosts(posts)
+  })
+
+  useEffect(() => {
+    fetchPosts()
+  }, [filter])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -36,7 +47,12 @@ function App() {
         filter={filter}
         setFilter={setFilter}
       />
-        <Postslist remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
+      {postError && 
+        <h1>Произошла ошибка ${postError}</h1>}
+      {isPostsLoading
+        ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}><Loader/></div>
+        : <Postslist remove={removePost} posts={sortedAndSearchedPosts} title="Посты про JS"/>
+      }
     </div>
   );
 }
